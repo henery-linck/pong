fe11bcdd8c779caaa69e02e64f35c1e54d49cdb5
Henery Linck Rosenhaim <henery@trinca.com>
2026-03-18 22:40:07 -0300

    Added top and bottom wall (invisible colliders)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,22 +7,22 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private InputActionAsset _inputActions;
     [SerializeField] private InputActionReference _moveAction;
 
-    private Rigidbody2D _rigidbody;
+    private Rigidbody2D _rigidBody;
 
     private void OnEnable() => _moveAction.action.Enable();
     private void OnDisable() => _moveAction.action.Disable();
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
         Vector2 input = _moveAction.action.ReadValue<Vector2>();
-        Vector2 movement = new Vector2(0, input.y);
+        Vector2 newPosition = new Vector2(_rigidBody.position.x, _rigidBody.position.y + input.y * _speed * Time.fixedDeltaTime);
 
-        transform.Translate(movement * _speed * Time.deltaTime);
+        _rigidBody.MovePosition(newPosition);
     }
 }
